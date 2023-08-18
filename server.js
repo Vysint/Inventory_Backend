760fb9c7816c478a8b9d3d577b8e4fa49f20cd41
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-18 23:34:58 +0300

    Cors() changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "https://inventory-app-vysint.vercel.app/",
+      "https://inventory-app-vysint.vercel.app",
       "http://localhost:3000",
     ],
     credentials: true,
