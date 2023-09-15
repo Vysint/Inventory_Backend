79bd27a25c6b805a2a75a0b7f65ee64b72050bbf
Vincent Kipngetich <109030133+Vysint@users.noreply.github.com>
2023-09-16 00:08:29 +0300

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -14,7 +14,7 @@ router.post("/", protect, upload.single("image"), createProduct);
 
 router.patch("/:id", protect, upload.single("image"), updateProduct);
 
-router.get("/",protect, getProducts);
+router.get("/", getProducts);
 
 router.get("/:id", protect, getSingleProduct);
 
